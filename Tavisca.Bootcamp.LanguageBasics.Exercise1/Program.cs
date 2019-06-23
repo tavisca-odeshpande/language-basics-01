686afcaef60674d4091d0aa79a06d48c5d643683
tavisca-odeshpande <52021545+tavisca-odeshpande@users.noreply.github.com>
2019-06-23 17:33:00 +0530

    solution added

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -22,8 +22,61 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            double[] integer_equation = new double[3];
+            double res_int;
+            int missing = 0,ind = 0,pass=0;
+            string unknown="", res_string;
+            string[] splitted_equation = equation.Split(new Char[] { '*', '=' });
+            for (int i = 0; i < 3; i++)
+            {
+                if (splitted_equation[i].IndexOf('?') < 0)
+                {
+                    string a = splitted_equation[i];
+                    integer_equation[i] = Int32.Parse(a);
+                }
+                else
+                {
+                    unknown = splitted_equation[i];
+                    ind = splitted_equation[i].IndexOf('?');
+                    missing = i;
+                }
+            }
+            if (missing == 0)
+            {
+                res_int = integer_equation[2] / integer_equation[1];
+                pass = CheckDecimal(res_int);
+            }
+            else if (missing == 1)
+            {
+                res_int = integer_equation[2] / integer_equation[0];
+                pass = CheckDecimal(res_int);
+            }
+            else
+            {
+                res_int = integer_equation[0] * integer_equation[1];
+                pass = CheckDecimal(res_int);
+            }
+            res_string = res_int.ToString();
+
+            if(res_string.Length==unknown.Length && pass==1)
+            {
+                return (int)Char.GetNumericValue(res_string, ind);
+            }
+            else
+            {
+                return -1;
+            }
         }
+
+        public static int CheckDecimal(double d)
+        {
+            if ((d % 1) > 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
     }
 }
